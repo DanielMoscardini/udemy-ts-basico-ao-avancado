472bb17fbe9b34631522d5902b992dfc5f2b6f62
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2022-10-23 21:30:57 -0300

    Iniciando secao 1 - Intruducao ao TS

---
+++ 01 - Introducao/index.ts
@@ -0,0 +1,21 @@
+const firstName = "Matheus";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log(`Olá ${name}!`);
+}
+
+greeting(firstName);
+// greeting(firstName);
+// greeting(firstName);
+
+
+
+function sum(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+greeting(firstName);
+// greeting(firstName);
+// greeting(firstName);
