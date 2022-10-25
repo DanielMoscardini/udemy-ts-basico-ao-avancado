87eff2ff92ea731bad94ee15d3e4e74a986b25d7
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2022-10-25 07:38:40 -0300

    Iniciando secao 4

---
+++ 04 - Narrowing/index.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+1. Typeof type guard.
+
+function sum(a: number | string, b: number | string) {
+  if (typeof a === "string" && typeof b === "string") {
+    console.log(parseFloat(a) + parseFloat(b));
+  } else if (typeof a === "number" && typeof b === "number") {
+    console.log(a + b);
+  } else {
+    console.log("Impossivel realizar a soma!");
+  }
+}
+
+sum(1, 4); // 5
+sum('1', '4'); // 5
+sum(1, '4'); // Impossivel realizar a soma!
+*/
