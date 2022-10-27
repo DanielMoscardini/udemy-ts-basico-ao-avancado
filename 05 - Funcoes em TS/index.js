22af9f55e0f82a9cb6ca32f4b3d42a3d4c8e2eb0
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2022-10-27 14:50:59 -0300

    Finalizando com generec functions

--- 05 - Funcoes em TS/index.js
+++ 05 - Funcoes em TS/index.js
@@ -1,2 +1,58 @@
 "use strict";
-alert('teste');
+/*
+1. Funcao sem retorno.
+
+function withoutReturn(): void {
+  console.log('Essa funcao nao tem retorno');
+}
+
+withoutReturn(); // Essa funcao nao tem retorno
+
+
+
+2. Callback sem argumento.
+
+function greeting(name: string) {
+  console.log(`Bem vindo ${name}`);
+}
+
+function preGreeting(f: (name: string) => string, userName: string) {
+  console.log(`Preparando a funcao`);
+
+  const greet = f(userName);
+
+  console.log(greet);
+}
+
+preGreeting(greeting, 'daniel');
+
+
+
+3. Generic functions.
+
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(firstElement([1, 2, true]));
+console.log(firstElement(['daniel', 4, false]));
+
+
+
+4. Constraint em generic functions.
+
+
+*/
+function biggestNumber(a, b) {
+    let biggest;
+    if (+a > +b) {
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    return biggest;
+}
+console.log(biggestNumber(1, 3));
+console.log(biggestNumber('6', '3'));
+// console.log(biggestNumber('6', 3));
