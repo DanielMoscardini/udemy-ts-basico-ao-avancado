fc4600731688fefc51d0debdd88bc95134f19e8f
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2022-11-07 12:07:02 -0300

    Iniciando secao 6 - Object Types

---
+++ 06 - Object Types/index.ts
@@ -0,0 +1,34 @@
+/*
+1. Interface como parametro.
+
+interface Product {
+  name: string;
+  price: number;
+  isAvailable: boolean;
+}
+
+function showProductDetails(product: Product) {
+  console.log(`O nome do produto é ${product.name} e seu preco é ${product.price}`);
+
+  if (product.isAvailable) {
+    console.log('O produto está disponivel');
+  } else {
+    console.log('O produto nao está disponivel.');
+  }
+}
+
+const shirt: Product = {
+  name: "Camiseta",
+  price: 19.99,
+  isAvailable: true
+}
+
+showProductDetails(shirt);
+
+
+
+2. 
+
+*/
+
+
